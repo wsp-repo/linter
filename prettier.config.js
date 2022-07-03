5ee35529293e0b544a495fca77d2195c7f0d3d9b
Artem Zuev <artem@wspro.ru>
2022-07-04 02:48:41 +0300

    Update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   arrowParens: 'always',
+  jsxBracketSameLine: false,
   printWidth: 80,
+  semi: true,
   singleQuote: true,
+  tabWidth: 2,
   trailingComma: 'all',
+  useTabs: false,
 };
